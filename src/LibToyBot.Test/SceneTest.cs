68828e8e22f47098ad5596fd9691d85df8c66f83
Argent78 <argent78@me.com>
2019-11-06 19:15:52 +0800

    Created xUnit test project

---
+++ src/LibToyBot.Test/SceneTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LibToyBot.Test
+{
+    public class SceneTest
+    {
+        [Fact]
+        public void TestSceneConstructor()
+        {
+
+        }
+    }
+}
